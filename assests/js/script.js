5c3cf477338602e9251ac313f9777daa6b8e6e19
Amanda Dell <amhernandez1163@gmail.com>
2022-03-01 23:35:53 -0600

    stopping point for night - added taskNameInput value to store input

--- assests/js/script.js
+++ assests/js/script.js
@@ -6,12 +6,16 @@ var createTaskHandler = function (event) {
     //tells browser to not immediately refresh page upon clicking a button. 
     event.preventDefault();
 
+    //when we use a square bracket in a selector, we're trying to select an HTML element by one of it's attributes - input element that has a name attribute set to task-name. We use single ' or else the string would fail
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //we used console.dir(taskNameInput); to see in the console where our data was being stored = GETTING | -the we added it to the var taskNameInput = SETTING
+
     //create a new task item to the DOM - adding li element under ul 
     var listItemEl = document.createElement("li");
     // style the new task item w selector style already being used in current css
     listItemEl.className = "task-item";
-    // adding text to the newly created li 
-    listItemEl.textContent = "This is a new task";
+    // adding text to the newly created li - it now equals taskNameInput - which is the new variable we created to store our input
+    listItemEl.textContent = taskNameInput;
      // appending this element to the task - no need to add to HTML
      tasksToDoEl.appendChild(listItemEl);
 }
