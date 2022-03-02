f86d050b6ac9ca633bc838d32ea36f125d5bdfc8
Amanda Dell <amhernandez1163@gmail.com>
2022-03-01 23:22:51 -0600

    added event object to function

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+//creating a variable for form let's us interact with the form and some of it's child HTML elements
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    //tells browser to not immediately refresh page upon clicking a button. 
+    event.preventDefault();
+
     //create a new task item to the DOM - adding li element under ul 
     var listItemEl = document.createElement("li");
     // style the new task item w selector style already being used in current css
     listItemEl.className = "task-item";
-    // adding text to the newly created item
+    // adding text to the newly created li 
     listItemEl.textContent = "This is a new task";
-     // appending this element to the task
+     // appending this element to the task - no need to add to HTML
      tasksToDoEl.appendChild(listItemEl);
 }
 
-//adding event listener to function above - because function is created, we can call it below - "on a button click, create a task"
-buttonEl.addEventListener("click", createTaskHandler); 
+//adding event listener to function above - because function is created, we can call it below - "when a user clicks a button element w a type attribute that has a value of submit, when the user presses Enter on keyboard" run fuction
+formEl.addEventListener("submit", createTaskHandler);
