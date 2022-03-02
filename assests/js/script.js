03a74a095e234efdbc0867c71a7a324587172a28
Amanda Dell <amhernandez1163@gmail.com>
2022-03-01 22:19:01 -0600

    add ability to add task

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,16 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-});
+var createTaskHandler = function () {
+    //create a new task item to the DOM - adding li element under ul 
+    var listItemEl = document.createElement("li");
+    // style the new task item w selector style already being used in current css
+    listItemEl.className = "task-item";
+    // adding text to the newly created item
+    listItemEl.textContent = "This is a new task";
+     // appending this element to the task
+     tasksToDoEl.appendChild(listItemEl);
+}
+
+//adding event listener to function above - because function is created, we can call it below - "on a button click, create a task"
+buttonEl.addEventListener("click", createTaskHandler); 
